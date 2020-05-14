12cb78418346582fece8de4cd4ce52597a535701
Iacobescu Tudor <tudor.iacobescu.1@gmail.com>
2020-05-14 11:39:09 +0300

    uh

--- lab12-the-final-showdown/main.sql
+++ lab12-the-final-showdown/main.sql
@@ -1,4 +1,4 @@
--- init
+-- initialize sequences and tables
 drop table test_answers;
 drop table test_entries;
 drop table tests;
@@ -6,22 +6,24 @@ drop sequence tests_seq;
 drop sequence test_entries_seq;
 drop sequence test_answers_seq;
 
+-- tests table, stores one test for each email
 create table tests
 (
     id    number primary key,
     email varchar2(100) unique,
-    score number
+    score number -- total test score
 );
 create sequence tests_seq start with 1;
 
+-- test entries table, stores the questions for each test
 create table test_entries
 (
     id              number primary key,
-    test_id         number,
-    question_id     varchar2(8),
-    answered        number(1) default 0 not null,
-    score           number,
-    correct_answers number,
+    test_id         number, -- test it belongs to
+    question_id     varchar2(8), -- the id of the question it presents in INTREBARI
+    answered        number(1) default 0 not null, -- 0 if not yet answered, 1 if already answered
+    score           number, -- the score obtained for the question
+    correct_answers number, -- the number of correct answers the test entry has, used for score calculation
     foreign key (test_id) references tests
 );
 create sequence test_entries_seq start with 1;
@@ -29,13 +31,13 @@ create sequence test_entries_seq start with 1;
 create table test_answers
 (
     id        number primary key,
-    entry_id  number,
-    answer_id varchar2(8),
+    entry_id  number, -- test entry with the question the answer is for
+    answer_id varchar2(8), -- the id of the answer in RASPUNSURI
     foreign key (entry_id) references test_entries (id)
 );
 create sequence test_answers_seq start with 1;
 
--- clear
+-- clear after tests
 delete
 from test_answers;
 delete
@@ -43,7 +45,10 @@ from test_entries;
 delete
 from tests;
 
--- main
+
+-- main section
+
+-- next_question function, as detailed on wiki
 create or replace function next_question(mail varchar2, answer varchar2) return varchar2 as
     test_exists              number;
     unanswered_entries_exist number;
@@ -54,11 +59,13 @@ create or replace function next_question(mail varchar2, answer varchar2) return 
     q_text                   varchar2(1000);
     v_return                 varchar2(4000);
 begin
+    -- stop immediately on null email
     if mail is null
     then
         return '';
     end if;
 
+    -- if a test for the email doesn't exist, make one
     select count(*)
     into test_exists
     from tests
@@ -69,11 +76,13 @@ begin
         make_test(mail);
     end if;
 
+    -- select the test
     select id
     into t_id
     from tests
     where email = mail;
 
+    -- process an answer if one was given
     if (answer is not null) then
         process_answer(t_id, answer);
     end if;
@@ -83,13 +92,16 @@ begin
     from tests
     where id = t_id;
 
+    -- check if there are any questions left to answer
     select count(*)
     into unanswered_entries_exist
     from test_entries
     where test_id = t_id
       and answered = 0;
 
-    if (unanswered_entries_exist > 0) then
+    if (unanswered_entries_exist > 0)
+        -- if there are, return the JSON for one to the user
+    then
         select id, question_id
         into te_id, q_id
         from test_entries
@@ -124,11 +136,15 @@ begin
         v_return := substr(v_return, 1, (length(v_return) - 1));
         v_return := v_return || '}]';
         return v_return;
+        -- otherwise, return the score for the test
     else
         return '{"result": ' || t_score || '}';
     end if;
 end;
 
+-- process the given answer for the test; it is assumed that the answer references a real question in the test
+--  and real answers to that question included in the given options
+-- answer format: 'Q###:A###,A###,A###'
 create or replace procedure process_answer(t_id number, answer varchar)
 as
     q_id        varchar2(8);
@@ -141,19 +157,20 @@ as
     ans_right   number := 0;
     ans_wrong   number := 0;
 begin
-    -- 203:16,32,12
+    -- get the question key, Q###
     q_id := regexp_substr(answer, 'Q[0-9]+', 1, 1);
 
-    dbms_output.put_line(answer || ' ... ' || q_id);
-
+    -- get info about the question (as a test_entry)
     select id, score, correct_answers, answered
     into te_id, te_score, te_corr, te_answered
     from test_entries
     where question_id = q_id;
 
+    -- if it hasn't already been answered, we answer it with the given answer IDs
     if te_answered = 0
     then
         for ans in (
+            -- select each A### answer id as a separate substring, feeding them into the loop
             select regexp_substr(answer, 'A[0-9]+', 1, level) as id
             from dual
             connect by regexp_substr(answer, 'A[0-9]+', 1, level) is not null
@@ -164,6 +181,7 @@ begin
                 from raspunsuri
                 where id = ans.id;
 
+                -- the answer is added to the number of right or wrong answers, accordingly
                 if corr = '1'
                 then
                     ans_right := ans_right + 1;
@@ -171,12 +189,14 @@ begin
                     ans_wrong := ans_wrong + 1;
                 end if;
             end loop;
+        -- the total score is calculated based on the number of correct answers the test entry had
         ans_score := 10 / te_corr;
         te_score := ans_score * (ans_right - ans_wrong);
         if te_score < 0 then
             te_score := 0;
         end if;
 
+        -- update the test entry and the score for the test as a whole
         update test_entries
         set score    = te_score,
             answered = 1
@@ -185,23 +205,25 @@ begin
     end if;
 end;
 
-create
-    or
-    replace procedure make_test(mail varchar2) as
+-- make a test for the given e-mail
+create or replace procedure make_test(mail varchar2) as
     v_q_id    varchar2(8);
     a_id      varchar2(8);
     t_id      number;
     te_id     number;
     v_cor_ans number;
 begin
+    -- get the new id, and insert a row into tests
     t_id := tests_seq.nextval;
     insert into tests values (t_id, mail, 0);
     for i in 1..10
         loop
+            -- use get_random_new_question to get a question that is not in the test yet
             v_q_id := get_random_new_question(t_id);
             te_id := test_entries_seq.nextval;
             insert into test_entries values (te_id, t_id, v_q_id, 0, null, null);
 
+            -- add one correct answer
             select id
             into a_id
             from (
@@ -215,6 +237,7 @@ begin
             insert into test_answers values (test_answers_seq.nextval, te_id, a_id);
 
             v_cor_ans := 1;
+            -- add 5 more random answers, avoiding duplicates
             for ans in (
                 select id, corect
                 from (
@@ -233,57 +256,48 @@ begin
                     end if;
                 end loop;
 
+            -- update the test entry with the right number of correct answers
             update test_entries set correct_answers = v_cor_ans where id = te_id;
         end loop;
 end;
 
-create
-    or
-    replace function get_random_new_question(t_id number)
+-- get the id for a question in a domain not already in the test
+create or replace function get_random_new_question(t_id number)
     return varchar2 as
     v_ret varchar2(8);
 begin
+    -- select one question from ...
     select id
     into v_ret
-    from intrebari
-    where domeniu not in (
-        select domeniu
-        from intrebari
-        where id in (
-            select question_id
-            from test_entries
-            where test_id = t_id
-        )
-    )
-      and rownum = 1;
+    from (
+             -- ... questions with the domain not in...
+             select id
+             from intrebari
+             where domeniu not in (
+                 -- ... the domains of questions ...
+                 select domeniu
+                 from intrebari
+                 where id in (
+                     -- ... already in the test
+                     select question_id
+                     from test_entries
+                     where test_id = t_id
+                 )
+             )
+             order by dbms_random.value()
+         )
+    where rownum = 1;
+
     return v_ret;
 end;
 
+
+-- test - answer should be changed each test to test the entire functionality
 declare
     v varchar2(4000);
 begin
-    v := next_question('test@test.test', 'Q16:A132,A133');
+    v := next_question('test@test.test', '');
+    -- example with answer:
+--     v := next_question('test@test.test', 'Q6:A53,A52');
     dbms_output.put_line(v);
-end;
-
-select *
-from test_entries
-         join intrebari on question_id = intrebari.id
-         join tests on test_entries.test_id = tests.id;
-
-select *
-from test_entries
-         join test_answers ta on test_entries.id = ta.entry_id;
-
-declare
-    answer varchar2(1000) := 'Q14:A116,A113,A120,A118';
-begin
-    for ans in (
-        select regexp_substr(answer, 'A[0-9]+', 1, level) as id
-        from dual
-        connect by regexp_substr(answer, 'A[0-9]+', 1, level) is not null
-        )
-        loop
-            dbms_output.put_line(ans.id);
-        end loop;
 end;
